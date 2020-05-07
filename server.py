ebd13f8eaa50b213c84aed25a4be88d5a9a46f30
Mészáros Gergely <goteguru@gmail.com>
2020-05-07 16:57:22 +0200

    template example, json communication

--- server.py
+++ server.py
@@ -1,16 +1,24 @@
-from flask import Flask, escape, request, jsonify
+from flask import Flask, escape, request, jsonify, render_template
 import config
 
 app = Flask(__name__)
 
+#-------------------- minta1 ------------------
 @app.route('/test')
 def hello():
     name = request.args.get("name", "World")
     return f'Hello, {escape(name)}!'
 
+#-------------------- minta2 ------------------
 @app.route('/jsontest')
 def jsontest():
     mydata = {'a': [1,2,3], 'b': 43}
     return jsonify( mydata )
+
+#-------------------- minta3 ------------------
+@app.route('/fromtemplate')
+def fromtemplate():
+    mydata = 'petike'
+    return render_template('template.html', mydata=mydata)
 
 app.run(host=config.host, port=config.port)
